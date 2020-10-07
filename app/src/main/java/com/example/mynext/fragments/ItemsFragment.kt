a8f9364a8826b07ad7163c1aa4d9e2e69be7cf70
FXStritt <fabien.stritt@gmail.com>
2020-10-07 11:59:44 +0200

    adds "find online" functionality

--- app/src/main/java/com/example/mynext/fragments/ItemsFragment.kt
+++ app/src/main/java/com/example/mynext/fragments/ItemsFragment.kt
@@ -1,7 +1,10 @@
 package com.example.mynext.fragments
 
 import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,7 +21,6 @@ import com.example.mynext.ui.ItemAdapter
 import com.example.mynext.util.ImageHelper
 import kotlinx.android.synthetic.main.dialog_item_details.view.*
 import kotlinx.android.synthetic.main.fragment_items.*
-import kotlinx.android.synthetic.main.item_card.view.*
 import java.text.DateFormat
 import java.util.*
 
@@ -29,7 +31,11 @@ class ItemsFragment : Fragment(), ItemClickListener {
     private val selectedCategory: SelectedCategoryViewModel by activityViewModels()
     private lateinit var itemsViewModel: ItemsViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         itemsViewModel = ViewModelProvider(this).get(ItemsViewModel::class.java)
 
@@ -41,6 +47,7 @@ class ItemsFragment : Fragment(), ItemClickListener {
 
         items_newitem_fab.setOnClickListener { findNavController().navigate(R.id.action_ItemsFragment_to_NewItemsFragment) }
 
+        //once fragment knows which category/items to display, initialize recyclerview
         selectedCategory.selected.observe(viewLifecycleOwner, { category ->
 
             val itemAdapter = ItemAdapter(this)
@@ -60,29 +67,61 @@ class ItemsFragment : Fragment(), ItemClickListener {
         })
     }
 
-    override fun onItemClickListener(item: Item) {
+    override fun onItemClickListener(item: Item) { //on item selection, create alert dialog showing item details & possible actions
         val dialogBuilder = AlertDialog.Builder(activity)
-        val dialogView = View.inflate(context,R.layout.dialog_item_details,null)
+        val dialogView = View.inflate(context, R.layout.dialog_item_details, null)
 
-        with(dialogView) {
-            itemdialog_title_tv.text = item.itemTitle
-            itemdialog_description_tv.text = item.description
-            itemdialog_recommender_tv.text = context.getString(R.string.recommended_by,item.recommender)
-
-            val dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault())
-            val formattedDate = dateFormat.format(item.dateCreated)
-            itemdialog_date_tv.text = context.getString(R.string.dateadded, formattedDate)
-
-            val bitmap = ImageHelper.retrieveBitmapFromFileSystem(context,item.imageName)
-            itemdialog_image_iv.setImageBitmap(bitmap)
-        }
+        setItemInformation(dialogView, item)
+        setButtonsActions(dialogView, item)
 
         dialogBuilder.setView(dialogView)
         val dialog = dialogBuilder.create()
         dialog.show()
     }
+
+    private fun setItemInformation(dialogView: View, item: Item) {
+        with(dialogView) {
+            itemdialog_title_tv.text = item.itemTitle
+            itemdialog_description_tv.text = item.description
+            itemdialog_recommender_tv.text = context.getString(
+                R.string.recommended_by,
+                item.recommender
+            )
+
+            val dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault())
+            val formattedDate = dateFormat.format(item.dateCreated)
+            itemdialog_date_tv.text = context.getString(R.string.dateadded, formattedDate)
+
+            val bitmap = ImageHelper.retrieveBitmapFromFileSystem(context, item.imageName)
+            itemdialog_image_iv.setImageBitmap(bitmap)
+        }
+    }
+
+    private fun setButtonsActions(dialogView: View, item: Item) {
+        with(dialogView) {
+            itemdialog_findonline_btn.setOnClickListener {
+                //Example of generated url: https://www.google.com/search?q=Book+To+kill+a+Mockingbird
+                val url = "https://www.google.com/search?q=${selectedCategory.selected.value?.itemsName}+${item.itemTitle}"
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(browserIntent)
+            }
+
+            itemdialog_markasdone_btn.setOnClickListener {
+                Log.d("MYTAG", "mark as done")
+            }
+
+            itemdialog_edit_btn.setOnClickListener {
+                Log.d("MYTAG", "edit item")
+            }
+
+            itemdialog_delete_btn.setOnClickListener {
+                Log.d("MYTAG", "delete item")
+            }
+
+        }
+    }
 }
 
 interface ItemClickListener {
-    fun onItemClickListener (item: Item)
+    fun onItemClickListener(item: Item)
 }
