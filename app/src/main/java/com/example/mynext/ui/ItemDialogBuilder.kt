487fc55e52e525a7e5c8e61a49bc1322ef00adfd
FXStritt <fabien.stritt@gmail.com>
2020-11-18 18:14:11 +0100

    removes delete button from item dialog (instead, swipe to delete)

--- app/src/main/java/com/example/mynext/ui/ItemDialogBuilder.kt
+++ app/src/main/java/com/example/mynext/ui/ItemDialogBuilder.kt
@@ -12,7 +12,6 @@ import com.example.mynext.fragments.ItemDialogCallback
 import com.example.mynext.model.Item
 import com.example.mynext.model.SelectedItemViewModel
 import com.example.mynext.util.ImageHelper
-import com.google.android.material.button.MaterialButton
 import kotlinx.android.synthetic.main.dialog_item_details.view.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -25,10 +24,6 @@ class ItemDialogBuilder(val context: Context, private val itemDialogCallback: It
 
         val dialogBuilder = AlertDialog.Builder(context)
         val dialogView = View.inflate(context, R.layout.dialog_item_details, null)
-
-        dialogView.setOnClickListener {
-            resetDeleteButton(dialogView.itemdialog_delete_btn)
-        }
 
         setItemInformation(dialogView, item)
 
@@ -87,8 +82,6 @@ class ItemDialogBuilder(val context: Context, private val itemDialogCallback: It
                 val url = "https://www.google.com/search?q=$itemsName+${item.itemTitle}"
                 val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
                 context.startActivity(browserIntent)
-
-                resetDeleteButton(dialogView.itemdialog_delete_btn)
             }
         }
 
@@ -107,8 +100,6 @@ class ItemDialogBuilder(val context: Context, private val itemDialogCallback: It
                     dialogView.item_dialog_layout.setBackgroundResource(R.drawable.background_gray)
                 }
                 itemDialogCallback.updateItem(item)
-
-                resetDeleteButton(dialogView.itemdialog_delete_btn)
             }
         }
 
@@ -117,29 +108,6 @@ class ItemDialogBuilder(val context: Context, private val itemDialogCallback: It
                 selectedItem.select(item)
                 navController.navigate(R.id.action_ItemsFragment_to_NewItemsFragment)
                 dialog.dismiss()
-                resetDeleteButton(dialogView.itemdialog_delete_btn)
-            }
-        }
-
-
-        with(dialogView.itemdialog_delete_btn) {
-            setOnClickListener {
-                if (tag == null) {
-                    tag = "delete"
-                    setBackgroundColor(resources.getColor(R.color.colorError, context.theme))
-                } else {
-                    itemDialogCallback.deleteItem(item)
-                    dialog.dismiss()
-                }
-            }
-        }
-    }
-
-    private fun resetDeleteButton(button: MaterialButton) {
-        with(button) {
-            if (tag != null) {
-                tag = null
-                setBackgroundColor(resources.getColor(R.color.colorAccent, context.theme))
             }
         }
     }
